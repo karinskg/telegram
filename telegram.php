c21f6958f2e2845e3c82ecef69560d73c78a6893
karinskg <154737383+karinskg@users.noreply.github.com>
2024-01-02 12:38:57 +0300

    Add files via upload

--- telegram.php
+++ telegram.php
@@ -8,7 +8,7 @@ $name = $_POST['user_name'];
 $phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
 $token = "5909066450:AAGv-z6-_sLGPcEIPlTFL4jd5dmMI7Kwzhs";
-$chat_id = " -4077255984";
+$chat_id = "-4077255984";
 $arr = array(
   'Имя пользователя: ' => $name,
   'Телефон: ' => $phone,
@@ -22,8 +22,8 @@ foreach($arr as $key => $value) {
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram) {
-  echo 'helo';
-} else {
-  echo "Error";
-}
+    header('Location: thankyou.html');
+  } else {
+    echo "Error";
+  }
 ?>
